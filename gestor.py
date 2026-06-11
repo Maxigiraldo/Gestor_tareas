e8d90a189fae888961d259145d0f3aa9dc8f2b90
max <140426723+Maxigiraldo@users.noreply.github.com>
2026-06-10 22:24:37 -0500

    fix: gestor logic and storage except

--- gestor.py
+++ gestor.py
@@ -1,6 +1,8 @@
+from models import Tarea
+
 class GestorTareas:
     def agregar(self, id, descripcion: str, prioridad: str, estado: str):
-        tarea = {'ID': id, 'Descripcion': descripcion, 'Prioridad': prioridad, 'Estado': estado}
+        tarea = Tarea(id, descripcion, prioridad, estado)
         return tarea
     
     def completar(self, id: int, data: list):
@@ -15,7 +17,13 @@ class GestorTareas:
             if data['ID'] == id:
                 data.remove(i)
     
-    def listar(self, data: list):
+    def listar(self, data: list, filtro):
         print("ID\tPRIORIDAD\tESTADO\tTAREA")
-        for i in data:
-            print(f"{data['ID']}\t{data['Prioridad']}\t{data['Estado']}\t{data['Descripcion']}")
+        for tarea in data:
+            if filtro is None or tarea[filtro[0]] == filtro[1]:
+                print(
+                f"{tarea['ID']}\t"
+                f"{tarea['Prioridad']}\t"
+                f"{tarea['Estado']}\t"
+                f"{tarea['Descripcion']}"
+                )
--- storage.py
+++ storage.py
@@ -8,6 +8,7 @@ class Storage:
                 return data
         except:
             print("No existe el archivo no se pudo abrir")
+            return None
     
     def guardar(self, data):
         with open('datos.json', 'w', encoding='utf-8') as archivo:
